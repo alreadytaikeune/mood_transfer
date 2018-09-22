b2c604aab57b6ff66ca5f22df65015cb7f52ce2e
alreadytaikeune <anis.khlif01@gmail.com>
2018-09-22 10:54:35 +0200

    First commit

---
+++ model.py
@@ -0,0 +1,11 @@
+#! encoding=utf8
+from __future__ import unicode_literals, print_function, absolute_import
+
+"""
+In this module are defined subclasses for the wavenet encoders and decoders
+that implement global conditioning.
+"""
+
+
+class WaveNet():
+    pass
